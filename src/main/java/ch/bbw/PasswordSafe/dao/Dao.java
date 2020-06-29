807db63344c1b3ff540a60e5c499922ca78ca6a1
C198455 <jeremy.senn@axa-winterthur.ch>
2020-06-29 14:41:28 +0200

    add Test-User for Rinaldo

--- src/main/java/ch/bbw/PasswordSafe/dao/Dao.java
+++ src/main/java/ch/bbw/PasswordSafe/dao/Dao.java
@@ -12,12 +12,11 @@ import ch.bbw.PasswordSafe.model.User;
 
 @Repository
 public class Dao {
-
-	
 	
     private List<User> users = Arrays.asList(
             new User(1, "lilJay", "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92"),
-            new User(2, "bigJay", "481f6cc0511143ccdd7e2d1b1b94faf0a700a8b49cd13922a70b5ae28acaa8c5"));
+            new User(2, "bigJay", "481f6cc0511143ccdd7e2d1b1b94faf0a700a8b49cd13922a70b5ae28acaa8c5"),
+    		new User(3, "RinaldoChef", "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92"));
 
     public Optional<User> getUserByCredentials(String hashedPw, String username) {
 //        return this.users.stream()
